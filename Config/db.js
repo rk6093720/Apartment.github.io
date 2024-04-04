82fdbcf94a660526afcd4ec8e782285836adab9a
Rohit Kumar <97551067+rk6093720@users.noreply.github.com>
2024-04-04 10:13:06 +0530

    4/4/24

--- Config/db.js
+++ Config/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const connection = mongoose.connect(process.env.DATABASE, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const connection = mongoose.connect(process.env.DATABASE
+  // , {
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
+// }
+);
 module.exports={connection}
